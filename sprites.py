6e18e55418a2bfdfecc0045bb3da413f491737cd
Kelvin Lai <57137749+Tha1widsol@users.noreply.github.com>
2020-11-11 16:07:36 +0000

    Add files via upload

---
+++ sprites.py
@@ -0,0 +1,67 @@
+import pygame
+
+enemywalk=[pygame.image.load("RobotL1.png"),pygame.image.load("RobotL2.png"),pygame.image.load("RobotL3.png"),pygame.image.load("RobotL4.png")]
+
+enemyattackleft=[pygame.image.load("RobotDamage1L.png"),pygame.image.load("RobotDamage1L.png"),pygame.image.load("RobotDamage1L.png"),pygame.image.load("RobotDamage1L.png"),pygame.image.load("RobotDamage1L.png"),pygame.image.load("RobotDamage2L.png")]
+enemyattackright=[pygame.image.load("RobotDamage1R.png"),pygame.image.load("RobotDamage1R.png"),pygame.image.load("RobotDamage1R.png"),pygame.image.load("RobotDamage1R.png"),pygame.image.load("RobotDamage1R.png"),pygame.image.load("RobotDamage2R.png")]
+
+
+############################################################################################################################################
+#Shooter
+shooterwalk=[pygame.image.load("shooterwalkleft1.png"),pygame.image.load("shooterwalkleft2.png"),pygame.image.load("shooterwalkleft3.png"),pygame.image.load("shooterwalkleft4.png"),]
+shootershootleft = [pygame.image.load("shootershootleft.png"),pygame.image.load("shootershootleft.png")]
+shootershootright = [pygame.image.load("shootershootright.png"),pygame.image.load("shootershootright.png")]
+
+
+
+############################################################################################################################################
+
+#red
+redwalking=[pygame.image.load("redwalking1.png"),pygame.image.load("redwalking2.png"),pygame.image.load("redwalking3.png"),pygame.image.load("redwalking4.png")]
+redattackingright=[pygame.image.load("redattackingright1.png"),pygame.image.load("redattackingright3.png")]
+redattackingleft =[pygame.image.load("redattackingleft1.png"),pygame.image.load("redattackingleft2.png"),pygame.image.load("redattackingleft3.png")]
+
+#Flyer
+flyerwalking=[pygame.image.load("flyerleft1.png"),pygame.image.load("flyerleft2.png"),pygame.image.load("flyerleft3.png"),pygame.image.load("flyerleft4.png"),pygame.image.load("flyerleft5.png")]
+flyerattackingleft=[pygame.image.load("flyerattackleft1.png"),pygame.image.load("flyerattackleft2.png"),pygame.image.load("flyerattackleft3.png"),pygame.image.load("flyerattackleft4.png"),pygame.image.load("flyerattackleft5.png"),pygame.image.load("flyerattackleft6.png")]
+flyerattackingright =[pygame.image.load("flyerattackright1.png"),pygame.image.load("flyerattackright2.png"),pygame.image.load("flyerattackright3.png"),pygame.image.load("flyerattackright4.png"),pygame.image.load("flyerattackright5.png"),pygame.image.load("flyerattackright6.png")]
+
+
+#User
+character = pygame.image.load("char.png")
+charshootright = pygame.image.load("charshootright.png")
+charshootleft = pygame.image.load("charshootleft.png")
+healthlogo = pygame.image.load("charshootright.png")
+charshootleft=[pygame.image.load("charshootleft.png"),pygame.image.load("charshootleftup.png")]
+charshootright=[pygame.image.load("charshootright.png"),pygame.image.load("charshootrightup.png")]
+walkright =[pygame.image.load("walkright1.png"),pygame.image.load("walkright2.png"),pygame.image.load("walkright3.png"),pygame.image.load("walkright4.png")]
+walkleft=[pygame.image.load("walkleft1.png"),pygame.image.load("walkleft2.png"),pygame.image.load("walkleft3.png"),pygame.image.load("walkleft4.png")]
+############################################################################################################################################
+#Extras
+damage=pygame.image.load("damage.png")
+
+
+	
+#######################################
+
+
+bosswalk= [pygame.image.load("bosswalkleft1.png"),pygame.image.load("bosswalkleft2.png"),pygame.image.load("bosswalkleft3.png"),pygame.image.load("bosswalkleft4.png")]
+bossfireleft = [pygame.image.load("bossfireleft1.png"),pygame.image.load("bossfireleft2.png")]
+bossfireright = [pygame.image.load("bossfireright1.png"),pygame.image.load("bossfireright2.png")]
+bossdeathleft=pygame.image.load("bossdeathleft.png")
+
+
+
+#####
+stevewalk = [pygame.image.load("stevewalk1.png"),pygame.image.load("stevewalk2.png"),pygame.image.load("stevewalk3.png"),pygame.image.load("stevewalk4.png")]
+steveattackleft =[pygame.image.load("steveattackleft.png"),pygame.image.load("steveattackleft.png")]
+steveattackright =[pygame.image.load("steveattackright.png"),pygame.image.load("steveattackright.png")]
+stevedeathleft=pygame.image.load("stevedeathleft.png")
+
+
+############
+signuswalk = [pygame.image.load("signus1.png"),pygame.image.load("signus2.png"),pygame.image.load("signus3.png")]
+signusattackleft= [pygame.image.load("signusattack1.png"),pygame.image.load("signusattack2.png"),pygame.image.load("signusattack3.png"),pygame.image.load("signusattack4.png")]
+signusattackright= [pygame.image.load("signusattack1.png"),pygame.image.load("signusattack2.png"),pygame.image.load("signusattack3.png"),pygame.image.load("signusattack4.png")]
+signusdeathleft= pygame.image.load("signus1.png")
+
