1e1c3aab96f805848e8f3c794de92d049287a276
Kelvin Lai <57137749+Tha1widsol@users.noreply.github.com>
2020-11-12 12:45:40 +0000

    Update ceasarcipher.py

--- ceasarcipher.py
+++ ceasarcipher.py
@@ -1,27 +1,24 @@
 def cipher(plaintext,key):
     newstring=""
-    encrypted_text=""
+    new_text=""
   
-    alphabet = ("abcdefghijklmnopqrstuvwxyz")
-
-    if plaintext.isupper():
-        alphabet=alphabet.upper()
+    characters = ("abcdefghijklmnopqrstuvwxyz1234567890")
         
-    for i in range(0,len(alphabet)):
-        if (i+key) > 25:
-            ciphertext = {alphabet[i]:alphabet[(i%(25-key+1))]}
+    for i in range(0,len(characters)):
+        if (i+key) > len(characters)-1:
+            ciphertext = {characters[i]:characters[(i%(25-key+1))]}
           
         else:
-            ciphertext = {alphabet[i]:alphabet[i+key]}
+            ciphertext = {characters[i]:characters[i+key]}
 
-        newstring+=ciphertext[alphabet[i]]
+        newstring+=ciphertext[characters[i]]
 
     for c in range(len(plaintext)):
-        for i in range(len(alphabet)):
-            if plaintext[c] == alphabet[i]:
-                encrypted_text+=newstring[i]
+        for i in range(len(characters)):
+            if plaintext[c] ==  characters[i]:
+                new_text+=newstring[i]
 
-    return encrypted_text
+    return new_text
 
 
 
